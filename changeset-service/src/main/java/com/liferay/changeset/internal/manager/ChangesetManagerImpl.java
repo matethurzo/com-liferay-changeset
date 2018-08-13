c78fd2d2be77edddfcf211083796d82535c37a3d
Máté Thurzó <mate.thurzo@liferay.com>
2018-08-13 13:55:21 +0200

    LPS-0 Add changeset manager

---
+++ changeset-service/src/main/java/com/liferay/changeset/internal/manager/ChangesetManagerImpl.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.changeset.internal.manager;
+
+import com.liferay.changeset.configuration.ChangesetConfiguration;
+import com.liferay.changeset.manager.ChangesetManager;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+
+/**
+ * @author Mate Thurzo
+ */
+@Component(immediate = true, service = ChangesetManager.class)
+public class ChangesetManagerImpl implements ChangesetManager {
+
+	@Override
+	public boolean isChangesetSupported(Class<?> clazz) {
+		if (_configurationsByResourceClass.containsKey(clazz) ||
+			_configurationsByVersionClass.containsKey(clazz)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean isChangesetSupported(String identifier) {
+		return _configurationsByIdentifier.containsKey(identifier);
+	}
+
+	@Reference(cardinality = ReferenceCardinality.MULTIPLE, unbind = "-")
+	private void addChangesetConfiguration(
+		ChangesetConfiguration<?, ?> changesetConfiguration) {
+
+		_configurationsByIdentifier.put(
+			changesetConfiguration.getIdentifier(), changesetConfiguration);
+		_configurationsByResourceClass.put(
+			changesetConfiguration.getResourceEntityClass(),
+			changesetConfiguration);
+		_configurationsByVersionClass.put(
+			changesetConfiguration.getVersionEntityClass(),
+			changesetConfiguration);
+	}
+
+	private final Map<String, ChangesetConfiguration<?, ?>>
+		_configurationsByIdentifier = new HashMap<>();
+	private final Map<Class<?>, ChangesetConfiguration<?, ?>>
+		_configurationsByResourceClass = new HashMap<>();
+	private final Map<Class<?>, ChangesetConfiguration<?, ?>>
+		_configurationsByVersionClass = new HashMap<>();
+
+}

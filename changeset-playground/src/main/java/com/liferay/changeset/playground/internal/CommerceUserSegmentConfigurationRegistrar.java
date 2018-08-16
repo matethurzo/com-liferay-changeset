80bce0bf2dceb5bfc98875200094c60d8978bac1
Máté Thurzó <mate.thurzo@liferay.com>
2018-08-16 16:06:15 +0200

    LPS-0 disable component for the time being

--- changeset-playground/src/main/java/com/liferay/changeset/playground/internal/CommerceUserSegmentConfigurationRegistrar.java
+++ changeset-playground/src/main/java/com/liferay/changeset/playground/internal/CommerceUserSegmentConfigurationRegistrar.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -23,13 +23,11 @@ import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 
 import java.util.Random;
 
-import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Mate Thurzo
  */
-@Component(immediate = true, service = ChangesetConfigurationRegistrar.class)
 public class CommerceUserSegmentConfigurationRegistrar
 	implements ChangesetConfigurationRegistrar
 		<CommerceUserSegmentEntry, CommerceUserSegmentEntry> {

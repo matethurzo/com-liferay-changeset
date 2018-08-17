9ed51df22e0e13bab244507a3458f5bc51a30a8b
Daniel Kocsis <daniel.kocsis@liferay.com>
2018-08-17 10:07:28 +0200

    LPS-0 Test

--- changeset-test/src/testIntegration/java/com/liferay/changeset/internal/baseline/manager/test/ChangesetCreationTest.java
+++ changeset-test/src/testIntegration/java/com/liferay/changeset/internal/baseline/manager/test/ChangesetCreationTest.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -16,16 +16,19 @@ package com.liferay.changeset.internal.baseline.manager.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.changeset.manager.ChangesetBaselineManager;
-import com.liferay.changeset.manager.ChangesetBaselineManagerUtil;
 import com.liferay.changeset.manager.ChangesetManager;
-import com.liferay.changeset.manager.ChangesetManagerUtil;
 import com.liferay.changeset.model.ChangesetBaselineCollection;
+import com.liferay.changeset.model.ChangesetBaselineEntry;
 import com.liferay.changeset.model.ChangesetCollection;
+import com.liferay.changeset.model.ChangesetEntry;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.service.test.ServiceTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.After;
@@ -49,6 +52,8 @@ public class ChangesetCreationTest {
 
 	@Before
 	public void setUp() throws Exception {
+		ServiceTestUtil.setUser(TestPropsValues.getUser());
+
 		_changesetManager.enableChangesets();
 	}
 
@@ -58,7 +63,23 @@ public class ChangesetCreationTest {
 	}
 
 	@Test
-	public void testCreateEmptyChangeset() throws Exception {
+	public void testCreateEmptyChangeset() {
+		final String name = RandomTestUtil.randomString();
+		final String descrtiption = RandomTestUtil.randomString();
+
+		long changesetCollectionId =
+			_changesetManager.create(name, descrtiption).map(
+				ChangesetCollection::getChangesetCollectionId).orElse(0L);
+
+		Assert.assertNotEquals(
+			"Changeset collection must not be null", changesetCollectionId, 0L);
+
+		List<ChangesetEntry> changesetEntries =
+			_changesetManager.getChangesetEntries(changesetCollectionId);
+
+		Assert.assertTrue(
+			"Changeset entries must be empty", changesetEntries.isEmpty());
+
 		Optional<ChangesetBaselineCollection> productionBaseline =
 			_changesetBaselineManager.getProductionBaseline();
 
@@ -66,21 +87,35 @@ public class ChangesetCreationTest {
 			"Production baseline must not be null",
 			productionBaseline.isPresent());
 
-		final String name = RandomTestUtil.randomString();
-		final String descrtiption = RandomTestUtil.randomString();
-
-		Optional<ChangesetCollection> changesetCollection =
-			_changesetManager.create(name, descrtiption);
+		Optional<ChangesetBaselineCollection> changesetBaseline =
+			_changesetBaselineManager.getChangesetBaselineCollection(
+				() -> name);
 
 		Assert.assertTrue(
-			"Changeset collection must not be null",
-			changesetCollection.isPresent());
+			"Changeset baseline must not be null",
+			changesetBaseline.isPresent());
+
+		List<ChangesetBaselineEntry> productionChangesetBaselineEntries =
+			_changesetBaselineManager.getChangesetBaselineEntries(
+				() -> productionBaseline.map(
+					ChangesetBaselineCollection::
+						getChangesetBaselineCollectionId).get());
+
+		List<ChangesetBaselineEntry> changesetBaselineEntries =
+			_changesetBaselineManager.getChangesetBaselineEntries(
+				() -> changesetBaseline.map(
+					ChangesetBaselineCollection::
+						getChangesetBaselineCollectionId).get());
+
+		Assert.assertEquals(
+			"Baselines must be equal", productionChangesetBaselineEntries,
+			changesetBaselineEntries);
 	}
 
 	@Inject
-	private ChangesetManager _changesetManager;
+	private ChangesetBaselineManager _changesetBaselineManager;
 
-	private final ChangesetBaselineManager _changesetBaselineManager =
-		ChangesetBaselineManagerUtil.getChangesetManager();
+	@Inject
+	private ChangesetManager _changesetManager;
 
 }

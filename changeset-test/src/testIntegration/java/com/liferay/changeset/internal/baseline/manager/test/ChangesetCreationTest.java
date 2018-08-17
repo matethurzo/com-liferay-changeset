3406ade9d9616d7aba44f5246b6d97d58a26bec0
Daniel Kocsis <daniel.kocsis@liferay.com>
2018-08-17 14:26:10 +0200

    LPS-0 Test

--- changeset-test/src/testIntegration/java/com/liferay/changeset/internal/baseline/manager/test/ChangesetCreationTest.java
+++ changeset-test/src/testIntegration/java/com/liferay/changeset/internal/baseline/manager/test/ChangesetCreationTest.java
@@ -15,13 +15,18 @@
 package com.liferay.changeset.internal.baseline.manager.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.test.util.BlogsTestUtil;
 import com.liferay.changeset.manager.ChangesetBaselineManager;
 import com.liferay.changeset.manager.ChangesetManager;
 import com.liferay.changeset.model.ChangesetBaselineCollection;
 import com.liferay.changeset.model.ChangesetBaselineEntry;
 import com.liferay.changeset.model.ChangesetCollection;
 import com.liferay.changeset.model.ChangesetEntry;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.service.test.ServiceTestUtil;
@@ -54,7 +59,14 @@ public class ChangesetCreationTest {
 	public void setUp() throws Exception {
 		ServiceTestUtil.setUser(TestPropsValues.getUser());
 
+		_serviceContext = new ServiceContext();
+
+		_serviceContext.setScopeGroupId(TestPropsValues.getGroupId());
+
 		_changesetManager.enableChangesets();
+
+		_productionChangsetBaselineCollection =
+			_changesetBaselineManager.getProductionBaseline().orElse(null);
 	}
 
 	@After
@@ -63,13 +75,60 @@ public class ChangesetCreationTest {
 	}
 
 	@Test
+	public void testCreateChangesetWithSupportedEntities() {
+	}
+
+	@Test
+	public void testCreateChangesetWithUnsupportedEntities() throws Exception {
+		Assert.assertFalse(
+			"Entity must not support chnageset feature",
+			_changesetManager.isChangesetSupported(JournalArticle.class));
+
+		final String name = RandomTestUtil.randomString();
+		final String descrtiption = RandomTestUtil.randomString();
+
+		Optional<ChangesetCollection> changesetCollectionOptional =
+			_changesetManager.create(name, descrtiption);
+
+		long changesetCollectionId = changesetCollectionOptional.map(
+			ChangesetCollection::getChangesetCollectionId).orElse(0L);
+
+		List<ChangesetEntry> changesetEntries =
+			_changesetManager.getChangesetEntries(changesetCollectionId);
+
+		Assert.assertEquals(
+			"Changes number must be 0", 0, changesetEntries.size());
+
+		_blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		changesetEntries = _changesetManager.getChangesetEntries(
+			changesetCollectionId);
+
+		Assert.assertEquals(
+			"Changes number must be 0", 0, changesetEntries.size());
+
+		Optional<ChangesetBaselineEntry> baselineEntry =
+			_changesetBaselineManager.getBaselineEntry(
+				_productionChangsetBaselineCollection.
+					getChangesetBaselineCollectionId(),
+				BlogsEntry.class.getName(), _blogsEntry.getEntryId());
+
+		Assert.assertFalse(
+			"Baseline entry must be null", baselineEntry.isPresent());
+	}
+
+	@Test
 	public void testCreateEmptyChangeset() {
 		final String name = RandomTestUtil.randomString();
 		final String descrtiption = RandomTestUtil.randomString();
 
-		long changesetCollectionId =
-			_changesetManager.create(name, descrtiption).map(
-				ChangesetCollection::getChangesetCollectionId).orElse(0L);
+		Optional<ChangesetCollection> changesetCollectionOptional =
+			_changesetManager.create(name, descrtiption);
+
+		long changesetCollectionId = changesetCollectionOptional.map(
+			ChangesetCollection::getChangesetCollectionId).orElse(0L);
 
 		Assert.assertNotEquals(
 			"Changeset collection must not be null", changesetCollectionId, 0L);
@@ -79,13 +138,6 @@ public class ChangesetCreationTest {
 
 		Assert.assertTrue(
 			"Changeset entries must be empty", changesetEntries.isEmpty());
-
-		Optional<ChangesetBaselineCollection> productionBaseline =
-			_changesetBaselineManager.getProductionBaseline();
-
-		Assert.assertTrue(
-			"Production baseline must not be null",
-			productionBaseline.isPresent());
 
 		Optional<ChangesetBaselineCollection> changesetBaseline =
 			_changesetBaselineManager.getChangesetBaselineCollection(
@@ -97,9 +149,8 @@ public class ChangesetCreationTest {
 
 		List<ChangesetBaselineEntry> productionChangesetBaselineEntries =
 			_changesetBaselineManager.getChangesetBaselineEntries(
-				() -> productionBaseline.map(
-					ChangesetBaselineCollection::
-						getChangesetBaselineCollectionId).get());
+				() -> _productionChangsetBaselineCollection.
+					getChangesetBaselineCollectionId());
 
 		List<ChangesetBaselineEntry> changesetBaselineEntries =
 			_changesetBaselineManager.getChangesetBaselineEntries(
@@ -112,10 +163,16 @@ public class ChangesetCreationTest {
 			changesetBaselineEntries);
 	}
 
+	@DeleteAfterTestRun
+	private BlogsEntry _blogsEntry;
+
 	@Inject
 	private ChangesetBaselineManager _changesetBaselineManager;
 
 	@Inject
 	private ChangesetManager _changesetManager;
+
+	private ChangesetBaselineCollection _productionChangsetBaselineCollection;
+	private ServiceContext _serviceContext;
 
 }

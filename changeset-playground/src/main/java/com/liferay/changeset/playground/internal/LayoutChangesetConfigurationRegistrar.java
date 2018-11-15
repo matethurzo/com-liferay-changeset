50b5f9a7a84ac4251ce406ebe837a20c6ed339cb
Gergely Mathe <gergely.mathe@liferay.com>
2018-11-15 15:08:02 +0100

    LPS-0 Actualize LayoutChangesetConfigurationRegistrar

--- changeset-playground/src/main/java/com/liferay/changeset/playground/internal/LayoutChangesetConfigurationRegistrar.java
+++ changeset-playground/src/main/java/com/liferay/changeset/playground/internal/LayoutChangesetConfigurationRegistrar.java
@@ -17,66 +17,56 @@ package com.liferay.changeset.playground.internal;
 import com.liferay.changeset.configuration.ChangesetConfiguration;
 import com.liferay.changeset.configuration.ChangesetConfigurationRegistrar;
 import com.liferay.changeset.cqrs.manager.ChangesetCQRSManager;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.model.LayoutVersion;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.service.LayoutLocalService;
-
 import com.liferay.portal.kernel.service.persistence.LayoutUtil;
+import com.liferay.portal.kernel.service.persistence.LayoutVersionUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 import java.util.List;
 
-// TODO Replace at marked places, once versioning code is created
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 
 /**
  * @author Gergely Mathe
  */
 @Component(immediate = true, service = ChangesetConfigurationRegistrar.class)
 public class LayoutChangesetConfigurationRegistrar
-	implements ChangesetConfigurationRegistrar<Layout, Layout> { // TODO Replace with <Layout, LayoutVersion>
+	implements ChangesetConfigurationRegistrar<Layout, LayoutVersion> {
 
 	@Override
 	public ChangesetConfiguration changesetConfiguration(
-		ChangesetConfiguration.Builder<Layout, Layout> builder) { // TODO Replace with <Layout, LayoutVersion>
+		ChangesetConfiguration.Builder<Layout, LayoutVersion> builder) {
 
 		return builder.identifier(
 			"layout"
 		).addResourceEntity(
-			Layout.class, LayoutUtil::fetchByPrimaryKey,
-			Layout::getPlid,
-			layout -> 0L,
-			// TODO Replace with Layout::getVersionId
-			_layoutLocalService
+			Layout.class, LayoutUtil::fetchByPrimaryKey, Layout::getPlid,
+			Layout::getVersionId, _layoutLocalService
 		).addVersionEntity(
-			Layout.class,
-			// TODO Replace with LayoutVersion.class
-			layout -> 0L,
-			// TODO Replace with LayoutVersion::getPlid
-			serializable -> null,
-			// TODO Replace with LayoutVersionUtil::fetchByPrimaryKey
-			layout -> 0L,
-			// TODO Replace with LayoutVersion::getVersionId
-			layout -> 1.0D,
-			// TODO Replace with LayoutVersion::getVersion
-			null, new Integer[] {WorkflowConstants.STATUS_APPROVED},
-			layout -> 0
-			// TODO Replace with LayoutVersion::getStatus
+			LayoutVersion.class, LayoutVersion::getPlid,
+			LayoutVersionUtil::fetchByPrimaryKey,
+			LayoutVersion::getLayoutVersionId, LayoutVersion::getVersion, null,
+			new Integer[]
+				{
+					WorkflowConstants.STATUS_APPROVED,
+					WorkflowConstants.STATUS_DRAFT
+				},
+			LayoutVersion::getStatus
 		).baselining(
 			() -> {
 				_changesetCQRSManager.disableCQRSRepository();
 
-				List<Layout> layouts = _layoutLocalService.getLayouts(
-					QueryUtil.ALL_POS, QueryUtil.ALL_POS);
-
-				// TODO Extend with fetching and returning latest versions
+				List<LayoutVersion> layoutVersions =
+					_layoutLocalService.getLayoutVersions();
 
 				_changesetCQRSManager.enableCQRSRepository();
 
-				return layouts;
+				return layoutVersions;
 			}
 		).indexer(
 			IndexerRegistryUtil::nullSafeGetIndexer
